a9c2519aaca304d26a2581c2cbd8d65b5326ad30
Sarah Williams <srwilliams8678@gmail.com>
2019-05-03 00:58:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -32,8 +32,16 @@ class Shoe
   def material
     @material
   end
+  
+  def condition=(condition)
+    @condition = condition
+  end
  
-  def turn_page
+  def condition
+    @condition
+  end
+ 
+  def cobble
     puts "Flipping the page...wow, you read fast!"
   end
  
